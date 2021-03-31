dcc3858535d75ebd30de2beab24253276ec2430a
markbegma <64325270+markbegma@users.noreply.github.com>
2021-03-31 14:32:58 +0300

    removed dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const settings_1 = require("./settings");
-const HDLPlatform_1 = require("./HDLPlatform");
-module.exports = (api) => {
-    api.registerPlatform(settings_1.PLATFORM_NAME, HDLPlatform_1.HDLBusproHomebridge);
-};
-//# sourceMappingURL=index.js.map
